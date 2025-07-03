7fb51edd9e0460c8369f565bc9a6314853c1a014
bhavanaJ-git6 <Jamalpur.Bhavana@cognizant.com>
2025-07-03 18:08:19 +0530

    leave commit

--- src/pages/LeaveBalancePage.jsx
+++ src/pages/LeaveBalancePage.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+
 import LeaveBalance from "../components/leave/LeaveBalance";
 
 const LeaveBalancePage = () => {
   return (
     <>
       <div className=" fs-1 fw-light">Leave Balance</div>
-      <LeaveBalance/>
+      <LeaveBalance />
     </>
   );
 };
